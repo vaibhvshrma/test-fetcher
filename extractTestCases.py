ffebe756e1190f54c30970f7749858bb9b209b77
Vaibhav Sharma <vaibhavinmit@gmail.com>
2020-06-13 23:29:10 +0530

    refactor code and add requirements file

--- extractTestCases.py
+++ extractTestCases.py
@@ -2,91 +2,85 @@ import sys
 import urllib.request
 from bs4 import BeautifulSoup
 
+
 def extractTestCases(urlOfQuestion):
-	#get Markup
-	try:
-		pg = urllib.request.urlopen(urlOfQuestion)
-	except Exception as e:
-		fh = open(sys.__stdout__,'w')
-		fh.write("An error occured while fetching the specified URL\n",e)
-		fh.flush()
-		fh.close()
+    # get Markup
+    try:
+        pg = urllib.request.urlopen(urlOfQuestion)
+    except Exception as e:
+        with open(sys.__stdout__, 'w') as fh:
+            fh.write("An error occured while fetching the specified URL\n", e)
 
-	#convert to BeautifulSoup
-	pg = BeautifulSoup(pg,'lxml')
+    # convert to BeautifulSoup
+    pg = BeautifulSoup(pg, 'lxml')
 
-	allTests = pg.find('div', class_ = 'sample-tests')
-	tcases = allTests.findAll('pre')
+    allTests = pg.find('div', class_='sample-tests')
+    tcases = allTests.findAll('pre')
 
-	#the number of testcases
-	T = len(tcases)//2
+    # the number of testcases
+    T = len(tcases) >> 1
 
-	#lists for input and output
-	ip = []
-	op = []
+    # lists for input and output
+    ip = []
+    op = []
 
-	for i in range(T):
-		ip.append(tcases[2*i].string)
-		op.append(tcases[(2*i)+1].string)
+    for i in range(T):
+        ip.append(tcases[2*i].string)
+        op.append(tcases[(2*i)+1].string)
 
-	#clean input output
-	ip = [i.strip() for i in ip if i is not None]
-	op = [i.strip() for i in op if i is not None]
+    # clean input output
+    ip = [i.strip() for i in ip if i is not None]
+    op = [i.strip() for i in op if i is not None]
 
-	#get question name
-	name = pg.find('div',class_ = 'header')
-	name = name.find('div',class_ = 'title')
-	name = name.string
-	name = name.split()			#remove spaces from in between
-	name = ''.join(name)
+    # get question name
+    name = pg.find('div', class_='header')
+    name = name.find('div', class_='title')
+    name = name.string
+    name = name.split()  # remove spaces from in between
+    name = ''.join(name)
 
+    # write to file
 
-	#write to file
+    sys.stdout = open(name + '.input', 'w')
 
-	sys.stdout = open(name + '.input','w')
+    print(T)  # total number of testcases
+    print(*ip, sep='\n')
 
-	print(T)		#total number of testcases
-	print(*ip,sep = '\n')
-
-	#now write expected output
-	sys.stdout = open(name + '.expectedOutput', 'w')
-
-	print(*op,sep = '\n')
+    # now write expected output
+    sys.stdout = open(name + '.expectedOutput', 'w')
+    print(*op, sep='\n')
 
 
 def getQuestionsList(urlOfContest):
-	#get Markup
-	try:
-		pg = urllib.request.urlopen(urlOfContest)
-	except Exception as e:
-		fh = open(sys.__stdout__,'w')
-		fh.write("An error occured while fetching the specified URL\n",e)
-		fh.flush()
-		fh.close()
+    # get Markup
+    try:
+        pg = urllib.request.urlopen(urlOfContest)
+    except Exception as e:
+        with open(sys.__stdout__, 'w') as fh:
+            fh.write("An error occured while fetching the specified URL\n", e)
 
-	#convert to BeautifulSoup
-	pg = BeautifulSoup(pg,'lxml')
+    # convert to BeautifulSoup
+    pg = BeautifulSoup(pg, 'lxml')
 
-	tbl = pg.find('table', class_ = 'problems')
+    tbl = pg.find('table', class_='problems')
 
-	listOfUrls = []
+    listOfUrls = []
 
-	for row in tbl.findAll('tr'):
-		if(row.find('td')):
-			listOfUrls.append('http://codeforces.com'+ row.a['href'])
+    for row in tbl.findAll('tr'):
+        if(row.find('td')):
+            listOfUrls.append('http://codeforces.com' + row.a['href'])
 
-	return listOfUrls
-		
+    return listOfUrls
 
-urlOfContest = input('Enter the link of the CodeForces contest whose testcases you want to fetch:\n')
+if __name__ == '__main__':
+    urlOfContest = input(
+        'Enter the link of the CodeForces contest'
+        'whose testcases you want to fetch:\n'
+    )
+    listOfQurls = getQuestionsList(urlOfContest)
+    for url in listOfQurls:
+        extractTestCases(url)
+    # restore stdout
+    sys.stdout = sys.__stdout__
 
-listOfQurls = getQuestionsList(urlOfContest)
-
-for url in listOfQurls:
-	extractTestCases(url)
-
-
-#restore stdout
-sys.stdout = sys.__stdout__
-
-print('Completed Successfully')
+    print('Completed Successfully')
